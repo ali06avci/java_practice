74edc63ade838fdc4344cb6698bfa22e7939624c
Ali AVCI <ali06avci@gmail.com>
2022-02-23 10:22:24 +0300

    added new classes

---
+++ src/practice/FindElementLocationOfList.java
@@ -0,0 +1,17 @@
+package practice;
+import java.util.*;
+public class FindElementLocationOfList {
+
+    public static void main(String[] args) {
+        //Get the location of the given words C, E in a list
+
+        List aList = new ArrayList();
+        aList.add("A");
+        aList.add("B");
+        aList.add("C");
+        aList.add("D");
+        aList.add("E");
+
+        System.out.println(aList.indexOf("C")+"\n"+aList.indexOf("E"));
+    }
+}
