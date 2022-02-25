34943ca1fcf254bdb7811bf4ee99013fd9d949da
Ali AVCI <ali06avci@gmail.com>
2022-02-25 16:46:18 +0300

    added for while loop classes

---
+++ src/practice/For_While_3_1.java
@@ -0,0 +1,23 @@
+package practice;
+import java.util.Arrays;
+public class For_While_3_1 {
+    public static void main(String[] args) {
+        String str="Javaisalsoeasy";
+        String output = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.lastIndexOf(str.charAt(i)) != str.indexOf(str.charAt(i))) {
+                if (output.indexOf(str.charAt(i)) == -1) {
+                    output += str.charAt(i)+",";
+                }
+            }
+        }
+
+        String[] chr = output.split(",");
+
+        System.out.println(Arrays.toString(chr));
+    }
+}
+
+
+
---
+++ src/practice/For_While_3_2.java
@@ -0,0 +1,29 @@
+package practice;
+public class For_While_3_2 {
+    public static void main(String[] args) {
+		int count=0;
+		String str = "Javaisalsoeasy";
+
+		char string[] = str.toCharArray();
+
+		System.out.print("Output:");
+
+		for(int i=0; i<string.length; i++) {
+			count=1;
+					for(int j=i+1; j<string.length; j++) {
+
+						if(string[i] == string[j] && string[i] != ' ') {
+							count++;
+							string[j]='0';
+						}
+
+					}
+					 if(count > 1 && string[i] != '0')
+
+						 System.out.print(string[i]);
+			}
+    }
+}
+
+
+
