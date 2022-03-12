12545282d8985c16ba2a09d9ca8438a92785a02d
Ali AVCI <ali06avci@gmail.com>
2022-03-12 11:54:14 +0300

    new oca questions added

---
+++ src/oca_questions/OCA_q4.java
@@ -0,0 +1,10 @@
+package oca_questions;
+public class OCA_q4 {
+	//How many "Hello World" outputs? !!!
+	public static void main(String[] args) {
+		for (int i=0;i<10;){
+			i=i++;
+			System.out.println(i+" Hello World");
+		}
+	}
+}
