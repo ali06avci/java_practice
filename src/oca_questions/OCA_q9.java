a3e89fc2791b0f85521dae068428a471c44b4c80
Ali AVCI <ali06avci@gmail.com>
2022-03-14 23:54:50 +0300

    updated

---
+++ src/oca_questions/OCA_q9.java
@@ -0,0 +1,19 @@
+package oca_questions;
+
+/*
+    Output?
+    A) Success
+    B) Failure
+    C)The code will not compile because of line 8
+    D)The code will not compile because of line 10
+ */
+
+public class OCA_q9 {
+    public static void main(String[] args) {
+        int x=0;
+        String s=null;
+
+        if (x == null) System.out.println("Success");
+        else System.out.println("Failure");
+    }
+}
