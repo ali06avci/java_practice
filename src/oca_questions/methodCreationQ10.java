6346826a149bda8d3921503246da4e86047bfe3c
Ali AVCI <ali06avci@gmail.com>
2022-04-05 03:09:36 +0300

    oca questions package updated

---
+++ src/oca_questions/methodCreationQ10.java
@@ -0,0 +1,15 @@
+package oca_questions;
+
+public class methodCreationQ10 {
+    public static void main(String[] args) {
+
+    }
+        //which methods work
+//    public void methodA(){return;}
+//    public void methodB(){return null;}
+//    public void methodD(){}
+//    public int methodD(){return 9;}
+//    public int methodE(){return 9.0;}
+//    public int methodF(){return;}
+//    public int methodG(){return null;}
+}
