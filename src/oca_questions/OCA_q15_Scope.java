6770b96584fd3d6a704b9149b466bc00a4d42705
Ali AVCI <ali06avci@gmail.com>
2022-04-29 16:30:13 +0300

    updated

---
+++ src/oca_questions/OCA_q15_Scope.java
@@ -0,0 +1,12 @@
+package oca_questions;
+
+public class OCA_q15_Scope {
+    public static  void main(String[] args){
+
+        do {
+            int y=1;
+            System.out.println(y++ + "");
+        }
+        while (y<=10);
+    }
+}
