7dab2c894f7c4b661ba51e4a690dc6ea3797a7ba
Ali AVCI <ali06avci@gmail.com>
2022-04-30 13:28:38 +0300

    added new class

--- src/oca_questions/OCA_q15_Scope.java
+++ src/oca_questions/OCA_q15_Scope.java
@@ -3,10 +3,10 @@ package oca_questions;
 public class OCA_q15_Scope {
     public static  void main(String[] args){
 
-        do {
-            int y=1;
-            System.out.println(y++ + "");
-        }
-        while (y<=10);
+//        do {
+//            int y=1;
+//            System.out.println(y++ + "");
+//        }
+//        while (y<=10);
     }
 }
