a9f827b44a61f61fc2b926916eef2aa567fe927e
Ali AVCI <ali06avci@gmail.com>
2022-05-07 19:44:06 +0300

    updated q17

--- src/oca_questions/OCA_q17_Arrays.java
+++ src/oca_questions/OCA_q17_Arrays.java
@@ -1,4 +1,15 @@
 package oca_questions;
 
+/*
+Which of the following array declarations is not correct
+*/
 public class OCA_q17_Arrays {
+    public static void main(String [] args){
+
+//        int[][] scores=new int[5][];
+//        Object[][][] cubbies = new Object[3][0][5];
+//        String beans []=new beans[6];
+//        int[][] types = new int[];
+//        int[][] java = new int[][];
+    }
 }
