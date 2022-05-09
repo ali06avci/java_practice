784471e8eda714460dc7ecf28c2d8a6b0c55dabc
Ali AVCI <ali06avci@gmail.com>
2022-05-09 23:50:28 +0300

    added new question

---
+++ src/oca_questions/OCA_q18_Arrays.java
@@ -0,0 +1,9 @@
+package oca_questions;
+//what will be the result if the code block is run
+public class OCA_q18_Arrays {
+    public static void main(String [] args){
+
+int arr[]={1,2,3,4};
+        System.out.println(arr instanceof Object);
+    }
+}
